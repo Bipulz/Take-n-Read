7405140d1c4eefce364f0ea9646a8e340c5c3dc7
Bipulz <bhandaribipul345@gmail.com>
2025-05-05 14:38:19 +0545

    updated schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,20 @@
+
 CREATE TABLE users (
     id INT(11) NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL,
     phno VARCHAR(15) NOT NULL,
     password VARCHAR(60) NOT NULL,
     address VARCHAR(255) DEFAULT NULL,
+    landmark VARCHAR(255) DEFAULT NULL,
     city VARCHAR(255) DEFAULT NULL,
     state VARCHAR(255) DEFAULT NULL,
     zip VARCHAR(255) DEFAULT NULL,
     accept_terms TINYINT(1) DEFAULT 0,
     profile_image VARCHAR(255) DEFAULT NULL,
-    PRIMARY KEY (id)
-);
-
+    PRIMARY KEY (id),
+    UNIQUE KEY (email)
+)
 
 CREATE TABLE Book (
     Bookid INT NOT NULL AUTO_INCREMENT,
@@ -24,27 +26,27 @@ CREATE TABLE Book (
     photo VARCHAR(255) DEFAULT NULL,
     user_email VARCHAR(100) NOT NULL,
     PRIMARY KEY (Bookid),
-    FOREIGN KEY (user_email) REFERENCES users(email)
-);
+    FOREIGN KEY (user_email) REFERENCES users(email) ON DELETE CASCADE
+)
 
 
 CREATE TABLE cart (
-    cartID INT NOT NULL AUTO_INCREMENT,
-    bookID INT NOT NULL,
-    userID INT NOT NULL,
+    cartID INT(11) NOT NULL AUTO_INCREMENT,
+    bookID INT(11) NOT NULL,
+    userID INT(11) NOT NULL,
     bookName VARCHAR(255) NOT NULL,
     author VARCHAR(100) NOT NULL,
     price DOUBLE NOT NULL,
     total_price DOUBLE NOT NULL,
+    quantity INT(11) DEFAULT 1,
     PRIMARY KEY (cartID),
-    FOREIGN KEY (bookID) REFERENCES Book(Bookid),
-    FOREIGN KEY (userID) REFERENCES users(id)
-);
-
-
+    KEY (bookID),
+    KEY (userID),
+    CONSTRAINT cart_ibfk_1 FOREIGN KEY (userID) REFERENCES users (id) ON DELETE CASCADE
+)
 
 CREATE TABLE book_order (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL AUTO_INCREMENT,
     order_id VARCHAR(100) NOT NULL,
     user_name VARCHAR(100) NOT NULL,
     address VARCHAR(255) NOT NULL,
@@ -55,4 +57,6 @@ CREATE TABLE book_order (
     price DOUBLE NOT NULL,
     payment VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
-);
+)
+
+
